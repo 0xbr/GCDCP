12e3d5d7af6d4a2ff1a981d52fa6cccb55c4cc02
0xbr <heliorocha@outlook.com>
2014-11-23 03:53:00 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,14 +2,9 @@ setwd(".\\UCI HAR Dataset")
 
 features <- read.table("features.txt")
 
-x_train <- read.table("./train/x_train.txt")
-x_test <- read.table("./test/x_test.txt")
-
-subject_train <- read.table("./train/subject_train.txt")
-subject_test <- read.table("./test/subject_test.txt")
-
-y_train <- read.table("./train/y_train.txt")
-y_test <- read.table("./test/y_test.txt")
+x_train <- read.table("./train/x_train.txt"); x_test <- read.table("./test/x_test.txt")
+subject_train <- read.table("./train/subject_train.txt"); subject_test <- read.table("./test/subject_test.txt")
+y_train <- read.table("./train/y_train.txt"); y_test <- read.table("./test/y_test.txt")
 
 x <- rbind(x_train, x_test); colnames(x) <- features$V2
 subject <- rbind(subject_train, subject_test); colnames(subject) <- c("Subject")
