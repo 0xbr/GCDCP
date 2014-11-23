08b739e94497ffce6d5ed6f20ede0303fc4cee5b
0xbr <heliorocha@outlook.com>
2014-11-23 04:04:44 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,20 @@
-setwd(".\\UCI HAR Dataset") # Set up working directory
+setwd("...\\UCI HAR Dataset") # Set up working directory (the dots must be replaced by the rest of the directory name)
 
+# Collecting pieces
 features <- read.table("features.txt")
 
 x_train <- read.table("./train/x_train.txt"); x_test <- read.table("./test/x_test.txt")
 subject_train <- read.table("./train/subject_train.txt"); subject_test <- read.table("./test/subject_test.txt")
 y_train <- read.table("./train/y_train.txt"); y_test <- read.table("./test/y_test.txt")
 
+# rbinding separately x, subject and activity (y)
+# naming the columns of x, subject and y
 x <- rbind(x_train, x_test); colnames(x) <- features$V2
 subject <- rbind(subject_train, subject_test); colnames(subject) <- c("Subject")
 y <- rbind(y_train, y_test); colnames(y) <- c("Activity")
 
+# cbinding x, subject and y
 sub_act <- cbind(subject, y)
 data_set_txt <- cbind(x, sub_act)
+
+write.table(data_set_txt, "data_set.txt") # Writing result in a txt file
