f4b07c14a7f9db1a1b32cc946d847825849365a3
0xbr <heliorocha@outlook.com>
2014-11-23 03:44:27 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,19 @@
 setwd(".\\UCI HAR Dataset")
 
-i <- read.table("features.txt")
+features <- read.table("features.txt")
 
-k <- read.table("./train/x_train.txt")
-j <- read.table("./test/x_test.txt")
+x_train <- read.table("./train/x_train.txt")
+x_test <- read.table("./test/x_test.txt")
 
-g <- read.table("./train/subject_train.txt")
-h <- read.table("./test/subject_test.txt")
+subject_train <- read.table("./train/subject_train.txt")
+subject_test <- read.table("./test/subject_test.txt")
 
-f <- read.table("./train/y_train.txt")
-e <- read.table("./test/y_test.txt")
+y_train <- read.table("./train/y_train.txt")
+y_test <- read.table("./test/y_test.txt")
 
-m <- rbind(k, j)
-colnames(m) <- i$V2
-n <- rbind(g, h)
-colnames(n) <- c("Subject")
-o <- rbind(f, e)
-colnames(o) <- c("Activity")
+x <- rbind(x_train, x_test); colnames(x) <- features$V2
+subject <- rbind(subject_train, subject_test); colnames(subject) <- c("Subject")
+y <- rbind(y_train, y_test); colnames(y) <- c("Activity")
 
-p <- cbind(n, o)
-q <- cbind(m, p)
+sub_act <- cbind(subject, y)
+data_set_txt <- cbind(x, sub_act)
